1d413bb74c641ab2fa59d511283c20a5daea243e
OMOCHI <satsukisama.mochi@gmail.com>
2023-10-06 22:44:06 +0900

    create Sword&Cleric class

--- src/Hero.java
+++ src/Hero.java
@@ -1,6 +1,7 @@
 public class Hero {
   String name;
   int hp;
+  Sword sword;
   public void sleep() {
     this.hp = 100;
     System.out.println(this.name + "は、眠って回復した！");
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
 public class Main {
   public static void main(String[] args) throws Exception {
+    Sword s = new Sword();
+    s.name = "炎の剣";
+    s.damage = 10;
     Hero h = new Hero();
     h.name = "ミナト";
     h.hp = 100;
-    System.out.println("勇者" + h.name + "を生み出しました！");
-
-    Matango m1 = new Matango();
-    m1.hp = 50;
-    m1.suffix = 'A';
-
-    Matango m2 = new Matango();
-    m2.hp = 48;
-    m2.suffix = 'B';
-
-    //冒険の始まり
-    h.slip();
-    m1.run();
-    m2.run();
-    h.run();
+    h.sword = s;
+    System.out.println(h.name + "の現在の武器は" + h.sword.name);
   }
 }
