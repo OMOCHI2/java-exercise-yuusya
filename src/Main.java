ee51c4a3f18bfd593f10213d787fd87b30930c9d
OMOCHI <satsukisama.mochi@gmail.com>
2023-10-06 22:59:29 +0900

    create Wizard class

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,15 @@ public class Main {
     h.hp = 100;
     h.sword = s;
     System.out.println(h.name + "の現在の武器は" + h.sword.name);
+
+    Hero h2 = new Hero();
+    h2.name = "アサカ";
+    h2.hp = 100;
+
+    Wizard w = new Wizard();
+    w.name = "スガワラ";
+    w.hp = 50;
+    w.heal(h);
+    w.heal(h2);
   }
 }
